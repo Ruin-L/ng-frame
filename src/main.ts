4dcddfd92e6fc342a571f38672e7a95769eed8ce
Ruin <1830275783@qq.com>
2022-03-04 16:19:41 +0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+/*
+ * @Description: {{ByRuin}}
+ * @Version: 2.0
+ * @Author: Ruin 🍭
+ * @Date: 2022-03-03 16:21:23
+ * @LastEditors: 刘引
+ * @LastEditTime: 2022-03-04 16:11:03
+ */
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
